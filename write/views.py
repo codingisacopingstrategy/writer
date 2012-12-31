c1e461d86e4747daf689641049a84297b7b8c7bd
Eric Schrijver <eric@ericschrijver.nl>
2012-12-31 19:12:43 +0100

    Too cool! A recursive template to render the comments
    thanks to http://blog.elsdoerfer.name/2008/01/22/recursion-in-django-templates/
    
    It’s pixel for pixel the same as the MT version.
    Just didn’t yet implement different author thumbnails,
    and commenter links

--- write/views.py
+++ write/views.py
@@ -20,9 +20,11 @@ def entry(request, slug):
         authors = MtAuthor.objects.filter(author_created_by=True) # This happens to get all the authors we need
         a_thumbnail = MtAsset.objects.get(pk = author.author_userpic_asset_id)
 
+        
+
         tpl_params = {}
         tpl_params['e'] = entry
-        tpl_params['e_permalink'] = '/or/' + slug
+        tpl_params['e_comments'] = MtComment.objects.filter(comment_visible=1).filter(comment_entry_id=entry.entry_id).order_by('comment_created_on')
         tpl_params['a'] = author
         tpl_params['a_thumbnail_url'] = a_thumbnail.asset_url % "http://mt.schr.fr/lib"
         tpl_params['a_entries'] = MtEntry.objects.filter(entry_author_id=author.author_id)
@@ -30,7 +32,7 @@ def entry(request, slug):
         tpl_params['authors'] =authors
         tpl_params['recent_entries'] = MtEntry.objects.all()[:10]
         tpl_params['recent_comments'] = MtComment.objects.filter(comment_visible=1)[:10]
-
+        tpl_params['parent'] = None
 
         return render_to_response("entry.html", tpl_params, context_instance = RequestContext(request))
 
