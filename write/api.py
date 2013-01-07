79aa815c592fa9bb34208816d038c8fa992092fc
Eric Schrijver <eric@ericschrijver.nl>
2013-01-08 00:47:38 +0100

    Update the entry-text through the front end: Aloha editor interfacing with the Tasty Pie

--- write/api.py
+++ write/api.py
@@ -1,9 +1,11 @@
 from tastypie.resources import ModelResource
+from tastypie.authorization import Authorization
 from write.models import MtEntry, MtComment
 
 class MtEntryResource(ModelResource):
     class Meta:
         queryset = MtEntry.objects.all()
+        authorization = Authorization()
         resource_name = 'entry'
 
 class MtCommentResource(ModelResource):
