71d0f478d5116f8a129a6418d0db2aa2f2f033ce
Eric Schrijver <eric@ericschrijver.nl>
2013-04-07 20:25:45 +0200

    Screenshot problems disappear when upgrading to phantomjs 1.9.0
    Also allow screenshotting specific entries

--- write/management/commands/screenshot.py
+++ write/management/commands/screenshot.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from django.core.management.base import BaseCommand
 from write.models import MtEntry
 from write.screenshots import screenshot
@@ -6,6 +9,17 @@ class Command(BaseCommand):
     help = 'Takes screenshots of unpublished entries'
 
     def handle(self, *args, **options):
-        local_drafts = [i.entry_slug() for i in MtEntry.objects.filter(entry_status = 1)]
-        screenshot(local_drafts)
+        """
+        Take screenshots of pages as shown on the development server.
+        
+            python manage.py screenshot
+        
+        By default it will shoot all unpublished entries.
+        If you want to specify an entry, specify it’s slug (multiple entries allowed)
+        
+            python manage.py screenshot "i-guess-this-is-a-unix-sin" "robin-gareus"
+        """
+        if len(args) == 0:
+            args = [i.entry_slug() for i in MtEntry.objects.filter(entry_status = 1)]
+        screenshot(args)
         self.stdout.write('Created screenshots of unpublished entries')
