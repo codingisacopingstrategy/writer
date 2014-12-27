bb63828cbd2a0fcd437058535a9c998bcb5a0319
Eric Schrijver <eric@ericschrijver.nl>
2014-12-27 11:45:16 +0100

    Make Settings compatible with Django 1.7

--- write/settings.py
+++ write/settings.py
@@ -13,8 +13,6 @@ ADMINS = (
     # ('Your Name', 'your_email@example.com'),
 )
 
-MANAGERS = ADMINS
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
@@ -82,13 +80,6 @@ STATICFILES_FINDERS = (
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = '@o_kaq$y1@&amp;kf2$z+@(fz-hto30$+#3lejj!3hxxi!+ekji&amp;*i'
 
-# List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
-)
-
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -97,7 +88,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     #'write.utils.TidyMiddleware'
     # Uncomment the next line for simple clickjacking protection:
-    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = TCP + (
@@ -110,7 +101,7 @@ ROOT_URLCONF = 'write.urls'
 WSGI_APPLICATION = 'write.wsgi.application'
 
 TEMPLATE_DIRS = (
-    os.path.join(PROJECT_DIR, '..', 'templates')
+    os.path.join(PROJECT_DIR, '..', 'templates'),
 )
 
 INSTALLED_APPS = (
