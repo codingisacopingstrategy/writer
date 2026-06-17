eab55e95006d34daf44d20430b5dba5b1ec93af0
Eric Schrijver <eric@ericschrijver.nl>
2026-06-17 17:07:55 +0200

    Update fabric and its fabfile to version 3
    Also upgrade paramiko ssh dep

--- fabfile.py
+++ fabfile.py
@@ -8,48 +8,59 @@ the movable type software is installed that takes care of publishing
 static html files, handling public comments and trackbacks.
 """
 
+from getpass import getpass
 from shlex import quote
-from fabric.api import run, cd, sudo, env, settings
+from fabric import Connection, task
 
 from write.local_settings import FABRIC_HOSTS, FABRIC_PATH, FABRIC_DJANGO_PATH
 
-env.hosts = FABRIC_HOSTS
-env.path = FABRIC_PATH
-env.django_path = FABRIC_DJANGO_PATH
+HOST = FABRIC_HOSTS[0] if isinstance(FABRIC_HOSTS, (list, tuple)) else FABRIC_HOSTS
+
+
+def connection():
+    return Connection(HOST)
 
 
 class FabricException(Exception):
     pass
 
 
-def deploy():
-    with cd(env.django_path):
-        run('git pull origin master')
-        sudo('supervisorctl restart tightpants')
+@task
+def deploy(c):
+    conn = connection()
+    with conn.cd(FABRIC_DJANGO_PATH):
+        conn.run('git pull origin master')
+        conn.sudo('supervisorctl restart tightpants', password=getpass(), pty=True)
 
 
-def status():
+@task
+def status(c):
     """
     Run `git status` on the server
     """
-    with cd(env.path):
-        run('git status')
+    conn = connection()
+    with conn.cd(FABRIC_PATH):
+        conn.run('git status')
 
 
-def pull():
+@task
+def pull(c):
     """
     Pull from GitHub to server
     """
-    with cd(env.path):
-        run('git pull origin master')
+    conn = connection()
+    with conn.cd(FABRIC_PATH):
+        conn.run('git pull origin master')
 
 
-def publish():
+@task
+def publish(c):
     """
     Rebuild the pages on the server
     """
-    with cd(env.path):
-        run('/home/s/apps/i.liketightpants.net/writer-venv/bin/python /home/s/apps/i.liketightpants.net/writer/manage.py publish')  # rebuild pages
+    conn = connection()
+    with conn.cd(FABRIC_PATH):
+        conn.run('/home/s/apps/i.liketightpants.net/writer-venv/bin/python /home/s/apps/i.liketightpants.net/writer/manage.py publish')  # rebuild pages
         """ I disabled making screenshots because even if the other pages don’t change,
             their new screenshots are never identical, so in git all these little png’s have
             to be updated.
@@ -59,39 +70,43 @@ def publish():
         # run('python screenshots.py') # generate screenshots for the archive
 
 
-def commit(slug=None, message=None):
+@task
+def commit(c, slug=None, message=None):
     """
     Commit on the server
     Specify the message in a command line argument as such:
-    fab publish:message="This is the commit message"
+    fab commit --message="This is the commit message"
     """
-    with cd(env.path), settings(abort_exception=FabricException):
+    conn = connection()
+    with conn.cd(FABRIC_PATH):
         # Add all posts’ html
-        run('ls *.html | grep -v googled | xargs git add')  # skip the Google Webmaster verification file google*.html
+        conn.run('ls *.html | grep -v googled | xargs git add')  # skip the Google Webmaster verification file google*.html
         if slug:
             # Add assets for this specific post
-            run('''cat ''' + slug + '''.html | python -c 'import re; import fileinput; r = re.compile(""""\/and\/(assets\/[^"]+)""" + chr(34)); print "\\n".join(["\\n".join(s.replace("/and/","") for s in r.findall(line)) for line in fileinput.input() if len(r.findall(line)) > 0])' | xargs git add ''')
+            conn.run('''cat ''' + slug + '''.html | python -c 'import re; import fileinput; r = re.compile(""""\\/and\\/(assets\\/[^"]+)""" + chr(34)); print "\\n".join(["\\n".join(s.replace("/and/","") for s in r.findall(line)) for line in fileinput.input() if len(r.findall(line)) > 0])' | xargs git add ''')
             # Screenshot this post, add it to git
             # run('/home/s/apps/i.liketightpants.net/writer-venv/bin/python manage.py screenshot %s' % slug)
-            run('git add assets/as/screenshots/of/%s.png' % slug)
+            conn.run('git add assets/as/screenshots/of/%s.png' % slug, warn=True)
             if message:
-                run('git commit -m %s' % quote(message))
+                conn.run('git commit -m %s' % quote(message))
             else:
-                try:
-                    run('git ls-files --error-unmatch %s.html' % slug)
-                    run('git commit -m "Modified post %s"' % slug)
-                except FabricException:
-                    run('git commit -m Added post %s"' % slug)
+                result = conn.run('git ls-files --error-unmatch %s.html' % slug, warn=True, hide=True)
+                if result.ok:
+                    conn.run('git commit -m "Modified post %s"' % slug)
+                else:
+                    conn.run('git commit -m "Added post %s"' % slug)
         elif message:
-            run('git commit -m %s' % quote(message))
+            conn.run('git commit -m %s' % quote(message))
 
 
-def archive():
+@task
+def archive(c):
     """
     Push from the server to GitHub
     """
-    with cd(env.path):
-        run('git push origin master')  # push to github
+    conn = connection()
+    with conn.cd(FABRIC_PATH):
+        conn.run('git push origin master')  # push to github
 
 
 """
